794eb75a45146314e89248e96616e01c26e975d5
Laasya <mlaasy16@gmail.com>
2025-09-25 12:59:33 +0530

    "Update index.html"

--- AstroRaids/vite.config.ts
+++ AstroRaids/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/AstroRaids/',   // 👈 this is the folder it will live in
+  base: './',   // 👈 this is the folder it will live in
   plugins: [react()],
 })
