2167dda656f4c793ade7873430274d8f540ed7c7
Laasya <mlaasy16@gmail.com>
2025-10-06 12:30:55 +0530

    "Updated games"

--- AstroRaids/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: './',   // 👈 this is the folder it will live in
-  plugins: [react()],
-})
